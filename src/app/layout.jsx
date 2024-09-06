08659d16ba8348590709ff993664edd9f4df5a7b
worshix <worshiplivingstone@gmail.com>
2024-09-06 04:03:08 +0200

    settup for material-ui

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,9 @@
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 export const metadata = {
   title: "Create Next App",
@@ -8,9 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-      >
+      <body>
+      <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+        <CssBaseline />
+        <Navbar />
         {children}
+        <Footer />
+      </AppRouterCacheProvider>
       </body>
     </html>
   );
