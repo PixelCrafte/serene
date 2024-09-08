16ca13e0023cbd869d7048304486dec22fb3a6b6
worshix <worshiplivingstone@gmail.com>
2024-09-08 17:16:22 +0200

    added more images

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
 const page = () => {
   return (
-    <main>
-      gallery
+    <main className='m-0 p-0 box-border'>
+      <div className='slider'>
+        <div className="slide-acive">
+          <img src="/images/" alt="something" className='object-cover'/>
+        </div>
+        <div className="slide-next">
+          <div className="slide-next-img">
+            <img src="" alt="" className='object-cover'/>
+          </div>
+        </div>
+      </div>
+
+      <div className="slider-content">
+        <div className="slider-content-active">
+          <h1 className='slider-text'>Tanatswa</h1>
+        </div>
+        <div className="sider-content-next">
+          <h1 className='slider-text'>Worship</h1>
+        </div>
+      </div>
     </main>
   )
 }
