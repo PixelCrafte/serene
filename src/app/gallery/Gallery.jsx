3c003b741b57f6814f68bcfb95289a0ef9e38138
worshix <worshiplivingstone@gmail.com>
2024-09-14 12:40:42 +0200

    changed Gallery

--- src/app/gallery/Gallery.jsx
+++ src/app/gallery/Gallery.jsx
@@ -1,6 +1,4 @@
-import dynamic from 'next/dynamic';
-
-const ImageFull = dynamic(() => import('./imageFull'), { ssr: false });
+import ImageFull from './ImageFull';
 
 const Gallery = () => {
   const images = [
